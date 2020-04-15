4b2429af97b605cef84117ceb5cc6d32d49ba575
Helain Schoonjans <helain.schoonjans@eurekanetwork.org>
2020-04-15 18:32:04 +0200

    Removed obsolete field

--- grails-app/controllers/fibonacci/FibonacciController.groovy
+++ grails-app/controllers/fibonacci/FibonacciController.groovy
@@ -3,8 +3,6 @@ package fibonacci
 import grails.converters.JSON
 
 class FibonacciController {
-    static responseFormats = ['json', 'xml']
-
     def show(Integer id) {
         def generator = new FibonacciGenerator()
         render([
